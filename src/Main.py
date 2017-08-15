221cac0f114b815c0f52ad51580583a95db577fa
Plínio Pereira <pliniojr@gmail.com>
2017-08-15 14:20:25 -0300

    self.CommInterface.AddressMotorChain() no __init__

--- src/Main.py
+++ src/Main.py
@@ -112,7 +112,7 @@ class PrototypeFilterWheelControl(QWidget):
 
         reply = QMessageBox.question(self, 'Message',
                                      "Are you sure to quit?", QMessageBox.Yes |
-                                     QMessageBox.No, QMessageBox.No)
+               çç                      QMessageBox.No, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
             event.accept()
